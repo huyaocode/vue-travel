bf523f8d9cae1d5e3a561bbd6c954d6d37a21b37
huyaocode <2385249676@qq.com>
2018-09-23 15:38:03 +0800

    添加配置

--- src/main.js
+++ src/main.js
@@ -8,7 +8,6 @@ import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
 
-
 //设置为 false 以阻止 vue 在启动时生成生产提示
 Vue.config.productionTip = false
 fastClick.attach(document.body)
